b61a8905f42ee609132c56e44d611e6570a2c7f6
Dean Harland <51263393+DeanHarland@users.noreply.github.com>
2025-10-07 09:08:30 +0100

    Completed To-do-list
    Completed the functionailty of the to do list.
    Needs Css still.

--- Project4JSminitasks/toDoListJS.js
+++ Project4JSminitasks/toDoListJS.js
@@ -12,8 +12,28 @@ function addToList(){
 
     const entry = document.createElement("li");
     entry.textContent = raw;
-    
+
+    const completeButton = document.createElement("button");
+    completeButton.addEventListener("click", () => setCompleted(entry));
+
+    const removeButton = document.createElement("button");
+    removeButton.addEventListener("click",() => removeEntry(entry));
+
+    entry.appendChild(completeButton);
+    entry.appendChild(removeButton);
     todolist.appendChild(entry);
     textinput.value = "";
+    completeButton.textContent = "Complete";
+    removeButton.textContent = "Remove";
     
+
+    
+}
+
+function setCompleted(entry){
+    entry.classList.toggle("done");
+}
+
+function removeEntry(entry){
+    entry.remove();
 }
